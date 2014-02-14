162108974173053d955076f7e680ce32e3bae29d
Tomas Dvorak <todvora@gmail.com>
2014-02-14 19:34:06 +0100

    initial version

---
+++ main.js
@@ -0,0 +1,64 @@
+google.load("feeds", "1");
+
+function initialize() {
+    var feed = new google.feeds.Feed("http://www.weblogy.cz/export/rss/");
+    feed.setNumEntries(50);
+    feed.load(function (result) {
+        if (!result.error) {
+            var container = document.getElementById("feed");
+            for (var i = 0; i < result.feed.entries.length; i++) {
+                var entry = result.feed.entries[i];
+                console.log(entry);
+                container.appendChild(createEntry(entry));
+            }
+        }
+    });
+}
+
+function createEntry(entry) {
+    var div = document.createElement("div");
+
+    var title = entry.title.split(" // ");
+
+    var heading = document.createElement("h1");
+    heading.appendChild(document.createTextNode(title[1]));
+
+    var text = document.createElement("p");
+    text.appendChild(document.createTextNode(entry.content));
+
+    var author = document.createElement("span");
+    author.appendChild(document.createTextNode(title[0]));
+    author.setAttribute("class", "author");
+
+    var date = document.createElement("span");
+    date.appendChild(document.createTextNode(formatDate(entry.publishedDate)));
+    date.setAttribute("class", "date");
+
+
+    div.appendChild(heading);
+    div.appendChild(text);
+    div.appendChild(author);
+    div.appendChild(date);
+
+    var link = document.createElement("a");
+    link.setAttribute("href", entry.link)
+
+    link.appendChild(div);
+    return link;
+}
+
+function formatDate(dateStr) {
+    var datum = new Date(dateStr);
+    var weekday = new Array("neděle", "pondělí", "úterý", "středa", "čtvrtek", "pátek", "sobota");
+    var result = "";
+    result += weekday[datum.getDay()] + " ";
+    result += datum.getDate() + ". ";
+    result += (1 + datum.getMonth()) + ". ";
+    result += datum.getFullYear() + ". ";
+    result += datum.getHours() + ":";
+    result += datum.getMinutes();
+    return result;
+}
+
+
+google.setOnLoadCallback(initialize);
